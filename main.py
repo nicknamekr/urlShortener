2d2914355a796baf7d1309096214cc92180885c4
Nickname <47962021+nicknamekr@users.noreply.github.com>
2022-09-08 05:29:07 +0000

    - ShortenerEngine -> StyleShortener Engine - Background Changes - Added rate limit (for REPLIT hosting)

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, redirect, request
 from replit import db
 import random, string
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask('app')
-shortener_page = 'https://sher.kro.kr/u/'
-port = int('3410')
+shortener_page = 'https://safeds.tk/u/'
+port = random.randint(10000, 40000)
+
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["50 per minute", "1 per second"],
+)
 
 key = ''
 for i in range(10) : key += random.choice(string.ascii_lowercase)
@@ -14,12 +22,18 @@ app.config["SECRET_KEY"] = key
 def index():
   return render_template('index.html')
 
+@app.route('/test')
+def test():
+  return render_template('example.html')
+
 @app.route('/u/<shorten>')
+@limiter.exempt
 def linking(shorten):
   value = db[shorten]
   return redirect(value)
 
 @app.route('/api', methods = ["GET", "POST"])
+@limiter.exempt
 def api():
   print(request.args.get('link', type = str))
   result = ""
@@ -28,4 +42,4 @@ def api():
   db[result] = request.args.get('link', type = str)
   return render_template('result.html', url=f"{shortener_page}{result}")
 
-app.run(host='0.0.0.0', port=port, debug=True)
+app.run(host='0.0.0.0', port=port, debug = True)
